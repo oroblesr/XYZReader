33d3eb89af99d54b9dfa21de44160cb737673b3b
oroblesr <oscar.roblesr@gmail.com>
2016-08-23 18:19:12 -0500

    Added basic transition and updated text style

--- app/src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
+++ app/src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
@@ -10,6 +10,7 @@ import android.graphics.Color;
 import android.graphics.Rect;
 import android.graphics.Typeface;
 import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.ShareCompat;
 import android.support.v7.graphics.Palette;
